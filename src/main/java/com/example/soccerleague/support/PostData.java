0064d53f0403b8a0cda8d06917af5e9cce245c29
impati <impati0716@icloud.com>
2022-06-08 20:48:56 +0900

    gitignore update

--- src/main/java/com/example/soccerleague/support/PostData.java
+++ src/main/java/com/example/soccerleague/support/PostData.java
@@ -12,6 +12,7 @@ import com.example.soccerleague.domain.director.Director;
 import com.example.soccerleague.springDataJpa.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
