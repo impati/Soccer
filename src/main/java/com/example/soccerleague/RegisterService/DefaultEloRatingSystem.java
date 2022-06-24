06621924512c585772f4ac2acc95ff3c02f68b1d
impati <impati0716@icloud.com>
2022-06-24 23:25:53 +0900

    refactoring ratingCalc with an internal class

--- src/main/java/com/example/soccerleague/RegisterService/DefaultEloRatingSystem.java
+++ src/main/java/com/example/soccerleague/RegisterService/DefaultEloRatingSystem.java
@@ -19,12 +19,15 @@ import com.example.soccerleague.springDataJpa.PlayerChampionsRecordRepository;
 import com.example.soccerleague.springDataJpa.PlayerLeagueRecordRepository;
 import com.example.soccerleague.springDataJpa.RoundRepository;
 import com.example.soccerleague.springDataJpa.TeamRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.image.ImageProducer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,24 +38,79 @@ import java.util.stream.Collectors;
 public class DefaultEloRatingSystem implements EloRatingSystem {
     private final PlayerLeagueRecordRepository playerLeagueRecordEntityRepository;
     private final PlayerChampionsRecordRepository playerChampionsRecordRepository;
-    private final TeamRepository teamEntityRepository;
+    private final TeamRepository teamRepository;
     private final LeagueTeamRecord leagueTeamRecord;
     private final CalculationRatingSupport calculationRatingSupport;
     private final RoundRepository roundRepository;
     private final Integer VI = 400;
 
 
-    private void teamRatingCalc(Team team ,double oppositeRating , MatchResult matchResult , Integer K){
-        double w = 0;
-        double a = 10;
-        double b = (oppositeRating - team.getRating()) / VI;
-        double we = 1 / (Math.pow(a,b) + 1);
-        if(matchResult.equals(MatchResult.WIN)) w = 1;
-        else if(matchResult.equals(MatchResult.DRAW)) w = 0.5;
-        double r = team.getRating() + K * (w - we);
-        r = Math.round(r * 100) / 100.0;
-        team.setRating(r);
+
+
+    @Data
+    class RatingInfo {
+        private List<PlayerRecord> playerRecords;
+        private Team team;
+        private double gradeAvg;
+        private double oppositeTeamAvg;
+        private int K;
+        private int score;
+        private MatchResult matchResult;
+
+        public RatingInfo(List<PlayerRecord> playerRecords,Team team, double gradeAvg, int K) {
+            this.playerRecords = playerRecords;
+            this.gradeAvg = gradeAvg;
+            K = K;
+            this.team = team;
+            matchResult =  this.playerRecords.get(0).getMathResult();
+        }
+        public void init(Team oppositeTeam){
+            oppositeTeamAvg  = oppositeTeam.getRating();
+        }
+        public void teamRatingCalc(){
+            double w = 0;
+            double a = 10;
+            double b = (oppositeTeamAvg - team.getRating()) / VI;
+            double we = 1 / (Math.pow(a,b) + 1);
+            if(matchResult.equals(MatchResult.WIN)) w = 1;
+            else if(matchResult.equals(MatchResult.DRAW)) w = 0.5;
+            double r = team.getRating() + K * (w - we);
+            r = Math.round(r * 100) / 100.0;
+            team.setRating(r);
+        }
+
+        public List<PlayerRecord> mappedPosition(Position [] positions){
+            List<PlayerRecord> ret = new ArrayList<>();
+            playerRecords.stream()
+                    .filter((pr)->
+                        Arrays.stream(positions).anyMatch(p->p.equals(pr.getPosition()))
+                    ).forEach(ele->ret.add(ele));
+
+            return ret;
+        }
+
+        private void playerRatingCalc(List<PlayerRecord> playerRecordList , double oppositeRating){
+            List<Player> players = new ArrayList<>();
+            playerRecordList.stream().forEach(ele->players.add(ele.getPlayer()));
+            players.stream().filter(ele->ele.getRating() == 0).forEach(ele->ele.setRating(1500));
+
+            for(int i =0;i<players.size();i++){
+                double myRating = players.get(i).getRating();
+                int myGrade = playerRecordList.get(i).getGrade();
+                double w = 0;
+                double a = 10;
+                double b = (oppositeRating - myRating) / VI;
+                double we = 1 / (Math.pow(a,b) + 1);
+                if(matchResult.equals(MatchResult.WIN))w = 1;
+                else if(matchResult.equals(MatchResult.DRAW)) w = 0.5;
+                double r = myRating + K * (w - we);
+                r += 3*((myGrade / gradeAvg) - 1);
+                r = Math.round(r * 100) / 100.0;
+                players.get(i).setRating(r);
+            }
+        }
     }
+
 
     @Override
     public void ratingCalc(Round round , List<PlayerRecord> plrA, List<PlayerRecord> plrB) {
@@ -67,16 +125,26 @@ public class DefaultEloRatingSystem implements EloRatingSystem {
         Team teamB = plrB.get(0).getTeam();
         if(teamB.getRating() == 0)teamB.setRating(1500);
 
+        RatingInfo ratingInfoA = new RatingInfo(plrA,teamA,avgGrade,K);
+        ratingInfoA.init(teamB);
+        ratingInfoA.teamRatingCalc();
 
+        RatingInfo ratingInfoB = new RatingInfo(plrB ,teamB,avgGrade,K);
+        ratingInfoB.init(teamA);
+        ratingInfoB.teamRatingCalc();
 
-        teamRatingCalc(teamA,teamB.getRating(),plrA.get(0).getMathResult(),K);
-        teamRatingCalc(teamB,teamA.getRating(),plrB.get(0).getMathResult(),K);
+        Position  position[][] = new Position[4][] ;
+        position[0] = new Position[] {Position.RF , Position.LF , Position.CF, Position.ST};
+        position[1] = new Position[] {Position.RM , Position.LM , Position.CM, Position.AM , Position.DM};
+        position[2] = new Position[] {Position.CB , Position.RB , Position.LB, Position.LWB ,Position.RWB};
+        position[3] = new Position[] {Position.GK};
 
-        calc(mappedDefenser(plrA),mappedDefenser(plrB),avgGrade);
-        calc(mappedMid(plrA),mappedMid(plrB),avgGrade);
-        calc(mappedStriker(plrA),mappedStriker(plrB),avgGrade);
-        calc(mappedGoalKeeper(plrA),mappedGoalKeeper(plrB),avgGrade);
-
+        for(int i = 0;i<4;i++) {
+            List<PlayerRecord> playerRecordA = ratingInfoA.mappedPosition(position[i]);
+            List<PlayerRecord> playerRecordB = ratingInfoB.mappedPosition(position[i]);
+            ratingInfoA.playerRatingCalc(playerRecordA, playerRecordB.stream().map(ele -> ele.getPlayer()).mapToDouble(s -> s.getRating()).average().orElse(0));
+            ratingInfoB.playerRatingCalc(playerRecordB, playerRecordA.stream().map(ele -> ele.getPlayer()).mapToDouble(s -> s.getRating()).average().orElse(0));
+        }
     }
 
 
@@ -93,14 +161,14 @@ public class DefaultEloRatingSystem implements EloRatingSystem {
 
             int value = 100;
             for (int i = 0; i < 6; i++) {
-                Team team = teamEntityRepository.findById(resp.get(i).getTeamId()).orElse(null);
+                Team team = teamRepository.findById(resp.get(i).getTeamId()).orElse(null);
                 team.setRating(team.getRating() + value);
                 value /= 2;
             }
 
             value = -2;
             for (int i = 10; i < resp.size(); i++) {
-                Team team = teamEntityRepository.findById(resp.get(i).getTeamId()).orElse(null);
+                Team team = teamRepository.findById(resp.get(i).getTeamId()).orElse(null);
                 team.setRating(team.getRating() + value);
                 value *= 2;
             }
@@ -111,136 +179,6 @@ public class DefaultEloRatingSystem implements EloRatingSystem {
 
     }
 
-
-
-    private void calc(List<PlayerRecord> plrA, List<PlayerRecord>plrB,double avgGrade){
-
-        List<Player> playerA =  new ArrayList<>();
-        List<Integer> gradeA = plrA.stream().map(ele->ele.getGrade()).collect(Collectors.toList());
-
-        plrA.stream().forEach(ele->{
-            playerA.add(ele.getPlayer());
-        });
-        playerA.stream().filter(ele->ele.getRating() == 0).forEach(ele->ele.setRating(1500));
-        Double avgRaingA = playerA.stream().mapToDouble(ele->ele.getRating()).average().orElse(0);
-
-
-        List<Player> playerB =  new ArrayList<>();
-        List<Integer> gradeB = plrB.stream().map(ele->ele.getGrade()).collect(Collectors.toList());
-
-        plrB.stream().forEach(ele->{
-            playerB.add(ele.getPlayer());
-        });
-
-        playerB.stream().filter(ele->ele.getRating() == 0).forEach(ele->ele.setRating(1500));
-        Double avgRaingB = playerB.stream().mapToDouble(ele->ele.getRating()).average().orElse(0);
-
-
-        for(int i =0;i<plrA.size();i++){
-            double myRating = playerA.get(i).getRating();
-            int myGrade = plrA.get(i).getGrade();
-            double w = 0;
-            double a = 10;
-            double b = (avgRaingB - myRating) / VI;
-            double we = 1 / (Math.pow(a,b) + 1);
-            MatchResult matchResult = plrA.get(i).getMathResult();
-
-            if(matchResult.equals(MatchResult.WIN)){
-                w = 1;
-            }
-            else if(matchResult.equals(MatchResult.DRAW)){
-                w = 0.5;
-            }
-            double r = myRating + K * (w - we);
-            r += 3*((myGrade / avgGrade) - 1);
-            r = Math.round(r * 100) / 100.0;
-            playerA.get(i).setRating(r);
-        }
-
-
-
-        for(int i =0;i<plrB.size();i++){
-            double myRating = playerB.get(i).getRating();
-            int myGrade = plrB.get(i).getGrade();
-            double w = 0;
-            double a = 10;
-            double b = (avgRaingA - myRating) / VI;
-            double we = 1 / (Math.pow(a,b) + 1);
-            MatchResult matchResult = plrB.get(i).getMathResult();
-
-            if(matchResult.equals(MatchResult.WIN)){
-                w = 1;
-            }
-            else if(matchResult.equals(MatchResult.DRAW)){
-                w = 0.5;
-            }
-            double r = myRating + K * (w - we);
-            r += 3*((myGrade / avgGrade) - 1);
-            r = Math.round(r * 100) / 100.0;
-            playerB.get(i).setRating(r);
-        }
-
-
-
-    }
-    private List<PlayerRecord> mappedDefenser(List<PlayerRecord> plr){
-
-        List<PlayerRecord> ret = new ArrayList<>();
-        plr.stream().forEach(ele-> {
-
-            Position position = ele.getPosition();
-            if (position.equals(Position.CB) || position.equals(Position.RB) ||
-                    position.equals(Position.LB) || position.equals(Position.LWB) || position.equals(Position.RWB)
-            ) {
-                ret.add(ele);
-            }
-
-        });
-        return ret;
-    }
-
-    private List<PlayerRecord> mappedStriker(List<PlayerRecord> plr){
-
-        List<PlayerRecord> ret = new ArrayList<>();
-        plr.stream().forEach(ele-> {
-            Position position = ele.getPosition();
-            if(position.equals(Position.ST )|| position.equals(Position.LF) ||
-                    position.equals(Position.RF) || position.equals(Position.CF)
-            ) {
-                ret.add(ele);
-
-            }
-        });
-        return ret;
-    }
-
-    private List<PlayerRecord> mappedGoalKeeper(List<PlayerRecord> plr){
-
-        List<PlayerRecord> ret = new ArrayList<>();
-        plr.stream().forEach(ele-> {
-            Position position = ele.getPosition();
-            if(position.equals(Position.GK)) {
-                ret.add(ele);
-
-            }
-        });
-        return ret;
-    }
-
-    private List<PlayerRecord> mappedMid(List<PlayerRecord> plr){
-
-        List<PlayerRecord> ret = new ArrayList<>();
-        plr.stream().forEach(ele-> {
-            Position position = ele.getPosition();
-            if(position.equals(Position.AM )|| position.equals(Position.LM) ||
-                    position.equals(Position.RM) || position.equals(Position.CM) || position.equals(Position.DM)
-            ) {
-                ret.add(ele);
-
-            }
-        });
-        return ret;
-    }
 
 
 }
